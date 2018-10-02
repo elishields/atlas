0b9de88003e37d10778de119ee090496d16b75ec
Turner Vink <turner@turnervink.com>
2018-10-02 12:53:28 -0700

    Added if statement to server script to run different logic on update vs initial load

--- src/server.js
+++ src/server.js
@@ -2,19 +2,23 @@
 
 (function () {
 
-    data.nodes = [];
-    var user = {};
+    if (input) { // server.update called
+        console.log('server.update called!');
+    } else { // Inital load
+        data.nodes = [];
+        var user = {};
 
-    var gr = new GlideRecord('sys_user');
-    gr.get(gs.getUserID());
+        var gr = new GlideRecord('sys_user');
+        gr.get(gs.getUserID());
 
-    user.key = gs.getDisplayValue('sys_id');
-    user.name = gr.getDisplayValue('name');
-    user.title = gr.getDisplayValue('title');
-    user.department = gr.getDisplayValue('department');
-    user.email = gr.getDisplayValue('email');
-    user.businessPhone = gr.getDisplayValue('business_phone');
-    user.mobilePhone = gr.getDisplayValue('mobile_phone');
-    user.location = gr.getDisplayValue('location');
-    data.nodes.push(user);
+        user.key = gs.getDisplayValue('sys_id');
+        user.name = gr.getDisplayValue('name');
+        user.title = gr.getDisplayValue('title');
+        user.department = gr.getDisplayValue('department');
+        user.email = gr.getDisplayValue('email');
+        user.businessPhone = gr.getDisplayValue('business_phone');
+        user.mobilePhone = gr.getDisplayValue('mobile_phone');
+        user.location = gr.getDisplayValue('location');
+        data.nodes.push(user);
+    }
 })();
