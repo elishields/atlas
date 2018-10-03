aa688aa5918bbe9acb82d2dce19a5b0839e60be7
elishields <elishields@gmail.com>
2018-10-02 22:34:59 -0700

    Created and set values for currentUser object in getCurrentUser() function.

--- src/server.js
+++ src/server.js
@@ -1,24 +1,34 @@
-// GlideUser API provides access to current user info
-
 (function () {
 
     if (input) { // server.update called
         console.log('server.update called!');
     } else { // Inital load
+
         data.nodes = [];
-        var user = {};
+        var currentUser = getCurrentUser();
+        data.nodes.push(currentUser);
+
+    }
+
+    function getCurrentUser() {
+
+        var currentUser = {};
 
         var gr = new GlideRecord('sys_user');
         gr.get(gs.getUserID());
 
-        user.key = gs.getDisplayValue('sys_id');
-        user.name = gr.getDisplayValue('name');
-        user.title = gr.getDisplayValue('title');
-        user.department = gr.getDisplayValue('department');
-        user.email = gr.getDisplayValue('email');
-        user.businessPhone = gr.getDisplayValue('business_phone');
-        user.mobilePhone = gr.getDisplayValue('mobile_phone');
-        user.location = gr.getDisplayValue('location');
-        data.nodes.push(user);
+        // returning undefined sys_id
+        currentUser.key = gs.getDisplayValue('sys_id');
+        currentUser.name = gr.getDisplayValue('name');
+        currentUser.title = gr.getDisplayValue('title');
+        currentUser.department = gr.getDisplayValue('department');
+        currentUser.email = gr.getDisplayValue('email');
+        currentUser.businessPhone = gr.getDisplayValue('business_phone');
+        currentUser.mobilePhone = gr.getDisplayValue('mobile_phone');
+        currentUser.location = gr.getDisplayValue('location');
+
+        console.log("returning currentUser: " + currentUser.name);
+        return currentUser;
     }
+
 })();
