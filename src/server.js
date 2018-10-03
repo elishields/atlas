792a9dc9dc29dd82696b25e3f3f6f9daecb34bf2
elishields <elishields@gmail.com>
2018-10-02 23:08:22 -0700

    User objects now contain manager attribute.

--- src/server.js
+++ src/server.js
@@ -28,6 +28,7 @@
         currentUser.businessPhone = gr.getDisplayValue('business_phone');
         currentUser.mobilePhone = gr.getDisplayValue('mobile_phone');
         currentUser.location = gr.getDisplayValue('location');
+        currentUser.manager = gr.getDisplayValue('manager');
 
         console.log("returning currentUser: " + currentUser.name);
         return currentUser;
@@ -41,6 +42,7 @@
         var gr = new GlideRecord('sys_user');
 
         gr.addActiveQuery();
+        // SELECT ... WHERE 'manager' == manager
         gr.addQuery('manager', manager);
         gr.query();
 
@@ -55,8 +57,20 @@
 
         var user = {};
 
+        gr.get(gs.getUserID());
 
+        // returning undefined sys_id
+        user.key = gs.getDisplayValue('sys_id');
+        user.name = gr.getDisplayValue('name');
+        user.title = gr.getDisplayValue('title');
+        user.department = gr.getDisplayValue('department');
+        user.email = gr.getDisplayValue('email');
+        user.businessPhone = gr.getDisplayValue('business_phone');
+        user.mobilePhone = gr.getDisplayValue('mobile_phone');
+        user.location = gr.getDisplayValue('location');
 
+        console.log("returning user: " + user.name);
+        return user;
     }
 
 })();
