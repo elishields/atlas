a4b337cc77d8e9a239e1394dbe95117209a441a4
j-omi <gomijoy@gmail.com>
2018-10-03 22:49:47 -0700

    Added missing information, changed the layout of each element in node (each section put into own panel)

--- src/client.js
+++ src/client.js
@@ -18,40 +18,65 @@ function ($scope, $location, $http, spUtil, $timeout) {
                     stroke: "darkblue", strokeWidth: 2
                 }),
             $(go.Panel, "Table",
-                {defaultAlignment: go.Spot.Left, margin: 4},
-                $(go.Picture,
-                    {
-                        row: 1, column: 0, rowSpan: 3, alignment: go.Spot.Center,
-                        margin: 10, width: 50, height: 50, background: "red"
-                    },
-                    new go.Binding("source")),
-                $(go.TextBlock, "Name: ",
-                    {row: 1, column: 2, margin: 10, font: "bold 12pt sans-serif"}),
-                $(go.TextBlock,
-                    {row: 1, column: 4, margin: 10},
-                    new go.Binding("text", "name")),
+                {defaultAlignment: go.Spot.Left, column: 10},
 
-                $(go.TextBlock, "Title: ",
-                    {row: 2, column: 2, font: "bold 12pt sans-serif"}),
-                $(go.TextBlock,
-                    {row: 2, column: 4, margin: 10},
-                    new go.Binding("text", "title")),
-
-                $("PanelExpanderButton", "addInfo",
-                    { column: 5 , row: 0}),
                 $(go.Panel, "Table",
-                    { name: "addInfo", row: 5, column: 1, columnSpan: 4, visible: false},
-                    $(go.TextBlock, "Email: ",
-                        {row: 6, column: 2, margin: 10, font: "bold 12pt sans-serif"}),
-                    $(go.TextBlock,
-                        {row: 6, column: 4, margin: 10},
-                        new go.Binding("text", "email")),
-                    $(go.TextBlock, "Phone: ",
-                        {row: 8, column: 2, margin: 10, font: "bold 12pt sans-serif"}),
-                    $(go.TextBlock,
-                        {row: 8, column: 4, margin: 10},
-                        new go.Binding("text", "business_phone")))
+                {defaultAlignment: go.Spot.Left, column: 0, row: 0},
+							
+                    $(go.Picture,
+                        {
+                            row: 0, column: 0, rowSpan: 3,
+                            margin: 10, width: 50, height: 50, background: "red"
+                        },
+                        new go.Binding("source"))
+                        
+                ),
 
+                $(go.Panel, "Table",
+                {defaultAlignment: go.Spot.Left, column: 1, row: 0},
+                                
+                    $(go.TextBlock,
+                        {row: 1, column: 0, margin: 10, font: "bold 12pt sans-serif"},
+                        new go.Binding("text", "name")),
+
+                    $(go.TextBlock,
+                        {row: 2, column: 0, margin: 10},
+                        new go.Binding("text", "title")),
+
+                    $("PanelExpanderButton", "addInfo",
+                        { column: 2, row: 0})
+                        
+                ),
+								
+							//HIDDEN INFO
+                $(go.Panel, "Table",
+                    {name: "addInfo", defaultAlignment: go.Spot.Left, column: 1, row: 1, visible: false},
+                
+                    $(go.TextBlock,
+                        {row: 0, column: 0, margin: 10}, 
+											new go.Binding("text", "department")),
+									
+									$(go.TextBlock,
+                        {row: 1, column: 0, margin: 10}, 
+											new go.Binding("text", "location")),
+									
+									$(go.TextBlock,
+                        {row: 2, column: 0, margin: 10}, 
+											new go.Binding("text", "email")),
+									
+									$(go.TextBlock, "Direct: ",
+                        {row: 3, column: 0, margin: 10}),
+									
+                   $(go.TextBlock,
+                       {row: 3, column: 0, margin: 5},
+                     new go.Binding("text", "business_phone")),
+									
+									$(go.TextBlock, "Mobile: ",
+                        {row: 4, column: 0, margin: 10}),
+									$(go.TextBlock,
+                        {row: 4, column: 1, margin: 5}, 
+											new go.Binding("text", "mobilePhone"))
+                )
             )
         );
 
