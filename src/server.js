45d9e21a7641f79578bb8948aa1f1a6c7d970fa0
elishields <elishields@gmail.com>
2018-10-15 14:33:47 -0700

    Added jsdoc comment for getReports().

--- src/server.js
+++ src/server.js
@@ -76,6 +76,14 @@
         user.mobilePhone = gr.getDisplayValue('mobile_phone');
         user.location = gr.getDisplayValue('location');
         user.parent = gr.getValue('manager');
+
+        user.hasReports = false;
+        if (getReports(user.key).length > 0) {
+            console.log("getting reports for " + user.name + " " + getReports(user.key) + " length is " + getReports(user.key).length);
+            user.hasReports = true;
+        }
+        console.log(user.name + " has reports " + user.hasReports);
+
         user.photo = gr.getDisplayValue('photo');
         if (user.photo.length < 1) {
             user.photo = null;
@@ -90,6 +98,7 @@
      * into the nodes array.
      *
      * @param {String} manager - the sys_id of the employee to get reports for
+     * @return {Array} the user objects that report to param manager
      */
     function getReports(manager) {
         console.log(manager);
